d082553df7980bb21fe6da78b68aded7a7ba899b
Sergey <segaoff@icloud.com>
2022-02-04 20:30:59 +0300

    исправил подсчет попыток и возможность отмены игры

--- main.js
+++ main.js
@@ -2,11 +2,19 @@ const isNumber = (num) => {
     return (!isNaN(parseFloat(num)) && isFinite(num) && !(/\s/.test(num)))
 }
 
+function getRandomArbitrary(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 let game = () => { 
-    let randomNum = 50;
+    let randomNum = getRandomArbitrary(1, 100);
+    console.log(randomNum);
+    let userNum = 0;
     let lives = 10;
     function result() {
-        lives--;
+        
         if (lives < 0) {
             if (confirm("Попытки закончились, хотите сыграть еще?")) {
                 game();
@@ -14,33 +22,33 @@ let game = () => {
                 return alert("До свидания!");
             }
         } else {
-            let userNum = +prompt("Угадай число от 1 до 100");
-            if (userNum === null) {
+            userNum = +prompt("Угадай число от 1 до 100");
+            if (userNum === 0) {
                 return alert("Игра окончена!");
-            }
-            if (isNumber(userNum)) {
-                if (userNum > randomNum) {
-                    alert("Загаданное число меньше, осталось попыток " + lives);
-                    result();
-                } else if (userNum < randomNum) {
-                    alert("Загаданное число больше, осталось попыток " + lives);
-                    result();
-                } else if (userNum === randomNum) {
-                    if (confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?")) {
-                        game();
-                    } else {
-                        return alert("Игра окончена!")
-                    }
-                } 
             } else {
-                alert("Введите число!")
-                result();
+                if (isNumber(userNum)) {
+                    lives--;
+                    if (userNum > randomNum) {
+                        alert("Загаданное число меньше, осталось попыток " + lives);
+                        result();
+                    } else if (userNum < randomNum) {
+                        alert("Загаданное число больше, осталось попыток " + lives);
+                        result();
+                    } else if (userNum === randomNum) {
+                        if (confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?")) {
+                            game();
+                        } else {
+                            return alert("Игра окончена!")
+                        }
+                    } 
+                } else {
+                    alert("Введите число!")
+                    result();
+                }
             }
         }
     }
     result();
 }
 
-let userNumber = game();
-
-userNumber();
+game();
